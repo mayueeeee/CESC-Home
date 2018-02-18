0068faccf58251c3f07e3174084afc096ccbfeca
Sarunyu Chankong <sarunyu.you@gmail.com>
2018-02-19 01:53:42 +0700

    Add Intro to index page

--- pages/index.js
+++ pages/index.js
@@ -6,30 +6,28 @@ import NavBar from '../components/home/NavBar'
 import Gallery from '../components/home/Gallery'
 import Contact from '../components/home/Contact'
 import Footer from '../components/home/Footer'
-
+import Intro from '../components/home/Intro'
 export default class extends React.Component {
-   
-   
-    render () {
-     
-      return (
-        <div>
-          <NavBar/>
-          
-           
-           
-            <h3>Register <Link href="/register"><a>Click!</a></Link></h3>
-            <Gallery/>
-            <Contact/>
-            <Contact/>
-            <Contact/>
-            <Contact/>
-            <Contact/>
-            <Contact/>
-            <Contact/>
-            <Footer/>
-       
-        </div>
-      )
-    }
+
+  render() {
+
+    return (
+      <div>
+        <Intro/>
+        <NavBar/>
+
+        
+        <Gallery/>
+        <Contact/>
+        <Contact/>
+        <Contact/>
+        <Contact/>
+        <Contact/>
+        <Contact/>
+        <Contact/>
+        <Footer/>
+
+      </div>
+    )
   }
+}
