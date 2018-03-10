44df3033c12f672dbca275c7cc032d4856ee7f9b
Sarunyu Chankong <sarunyu.you@gmail.com>
2018-03-11 02:26:32 +0700

    Complete timeline section

--- components/home/Timeline.js
+++ components/home/Timeline.js
@@ -4,13 +4,37 @@ import {Container, Row, Col} from 'reactstrap';
 import SectionHeader from './SectionHeader'
 
 const TimelineWarper = styled.div `
-    padding: 100px 0 100px 0;
+    //padding: 2vh 0 -2vh 0;
     background-image: url("/static/images/bg/bg_timeline_png.png");
     background-position: center;
     background-repeat: no-repeat;
     background-size: cover;
+    margin-bottom: -23vh;
+`
+const StepText = styled.p `
+    font-family: "superspace_regularregular";
+    font-size: 1.5rem;
+`
+const StepIcon = styled.img `
+    width: 16vh;
+    margin-top: 2vh;
+    margin-bottom: 2vh;
 `
 
+const PeriodText = styled.div `
+    font-family: "superspace_regularregular";
+    font-size: 1.25rem;
+`
+
+const PolygonWarper = styled.div `
+  margin-top: -5vh;
+  z-index: 3;
+
+` 
+const Polygon = styled.img `
+  width: 50%;
+
+`
 export default class Timeline extends React.Component {
 
   render() {
@@ -19,14 +43,86 @@ export default class Timeline extends React.Component {
       <TimelineWarper className="text-center">
         <Container fluid={true}>
           <Row>
-            <Col xs={{ size: 10, offset: 1 }}>
+            <Col xs={{
+              size: 10,
+              offset: 1
+            }}>
               <SectionHeader title="Timeline"/>
-              
-
+            </Col>
+          </Row>
+          <Row>
+            <Col xs={12} md={{
+              size: 2,
+              offset: 2
+            }}>
+              <Row>
+                <Col><StepIcon src="/static/images/step/icon_apply.svg"/></Col>
+              </Row>
+              <Row>
+                <Col>
+                  <PeriodText>20 มีนาคม - 7 เมษายน</PeriodText>
+                </Col>
+              </Row>
+              <Row>
+                <Col>
+                  <StepText>รับสมัคร</StepText>
+                </Col>
+              </Row>
             </Col>
 
+            <Col xs={12} md={2}>
+              <Row>
+                <Col><StepIcon src="/static/images/step/icon_announce.svg"/></Col>
+              </Row>
+              <Row>
+                <Col>
+                  <PeriodText>24 เมษายน</PeriodText>
+                </Col>
+              </Row>
+              <Row>
+                <Col>
+                  <StepText>ประกาศผล</StepText>
+                </Col>
+              </Row>
+            </Col>
+            <Col xs={12} md={2}>
+              <Row>
+                <Col><StepIcon src="/static/images/step/icon_confirm_2.svg"/></Col>
+              </Row>
+              <Row>
+                <Col>
+                  <PeriodText>24 เมษายน - 26 เมษายน</PeriodText>
+                </Col>
+              </Row>
+              <Row>
+                <Col>
+                  <StepText>ยืนยันสิทธิ์</StepText>
+                </Col>
+              </Row>
+            </Col>
+            <Col xs={12} md={2}>
+              <Row>
+                <Col><StepIcon src="/static/images/step/icon_camp.svg"/></Col>
+              </Row>
+              <Row>
+                <Col>
+                  <PeriodText>25 มีนาคม - 8 เมษายน</PeriodText>
+                </Col>
+              </Row>
+              <Row>
+                <Col>
+                  <StepText>วันค่าย</StepText>
+                </Col>
+              </Row>
+            </Col>
           </Row>
+
+         
         </Container>
+        <PolygonWarper>
+          <Polygon src ="/static/images/polygon/dec4_1.svg"/>
+          <Polygon src ="/static/images/polygon/dec5_1.svg"/>
+        </PolygonWarper>
 
       </TimelineWarper>
     )
