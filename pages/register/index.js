da1f634e76bb328e16a8c81a1c5f334b68d1b0c7
Sarunyu Chankong <sarunyu.you@gmail.com>
2018-03-23 05:19:51 +0700

    Add register page

---
+++ pages/register/index.js
@@ -0,0 +1,91 @@
+import React from 'react'
+import Link from 'next/link'
+import styled from 'styled-components';
+import axios from 'axios'
+import {Container, Row, Col} from 'reactstrap';
+import RegForm from '../../components/register/form'
+import CampSelector from '../../components/register/SubCampSelector'
+import SectionHeader from '../../components/home/SectionHeader'
+import Alert from '../../components/Core/Alert'
+import setting from '../../config.json'
+const Logo = styled.img `
+  z-index = 3;
+  width:80%; 
+  margin-top: 10vh;
+
+  /*For Desktop*/
+  @media (min-width: 1025px){
+    max-width: 25%;
+    margin-top: 5vh;
+
+  }
+`
+const FormHeader = styled.h3 `
+  // font-family: "superspace_regularregular";  
+  font-family: 'Mitr', sans-serif;
+  font-weight:400;
+  margin-top: 20px;
+`
+
+const Label = styled.label `
+  font-family: 'Mitr', sans-serif;
+  font-size: 1.25rem;
+  font-weight:300;
+`
+const PageWarper = styled.div `
+  padding-bottom: 6vh;
+  background-image: url("/static/images/applyForm/bg.png");
+  background-position: center;
+  background-repeat: no-repeat;
+  background-size: cover;
+`
+export default class Register extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      isError: false,
+      errorTXT: "",
+      form: {}
+    }
+
+  }
+
+  componentDidMount() {
+    console.log(localStorage.access_token)
+    axios
+      .post(setting.prod_api_root + '/web/profile', {
+        access_token: localStorage.access_token        
+      })
+      .then(res=>{
+        // console.log(res)
+        this.setState({form:res.data})
+        // console.log(this.state)
+      })
+      .catch(err=>{
+        console.log(err.response.data.message)
+        this.setState({isError:true,
+        errorTXT: err.response.data.message})
+      });
+
+  }
+  render() {
+    return (
+      <PageWarper>
+        <Container>
+          <Row className="text-center">
+            <Col>
+              <Logo src="/static/images/hero/logo@4x.png"/>
+              <SectionHeader title="Registration form"/>
+            </Col>
+          </Row>
+          { this.state.isError ? <Alert color="danger" errorCode={this.state.errorTXT}/> : ""}
+          {/* <Alert color="danger" errorCode={this.state.errorTXT}/> */}
+          {this.state.form.sub_camp==null ? <CampSelector/> : <RegForm form={this.state.form}/> }
+          
+          
+        </Container>
+
+      </PageWarper>
+    )
+  }
+}
