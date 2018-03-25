0e4374cd8db64694360ae8acc116a8a6d2131003
Sarunyu Chankong <sarunyu.you@gmail.com>
2018-03-26 02:46:39 +0700

    Dashboard V.2

--- pages/dashboard.js
+++ pages/dashboard.js
@@ -2,12 +2,19 @@ import React from 'react'
 import Link from 'next/link'
 import Router from 'next/router'
 import styled, {keyframes} from 'styled-components';
-import {Button, Container, Row, Col, Input} from 'reactstrap';
-import Login from '../pages/register/login'
-import RegForm from '../components/register/form'
+import {
+  Button,
+  Container,
+  Row,
+  Col,
+  Card,
+  CardBody,
+  Alert
+} from 'reactstrap';
 import axios from 'axios'
 import setting from '../config.json'
 import Layout from '../components/Layout'
+import SectionHeader from '../components/home/SectionHeader'
 const Logo = styled.img `
   z-index = 3;
   width:80%; 
@@ -26,7 +33,29 @@ const FormHeader = styled.h3 `
   font-weight:400;
   margin-top: 20px;
 `
+const PageWarper = styled.div `  
+  background-image: url("/static/images/applyForm/bg.png");
+  background-position: center;
+  background-repeat: no-repeat;
+  background-size: cover;
+  height: 100vh;
+`
 
+const CardRow = styled(Row)`
+  margin-bottom: 10px;
+`
+const RoundButton = styled(Button)`
+  border-radius: 100px;
+  font-family: 'Mitr', sans-serif;
+  font-weight:300;
+  text-decoration:none;
+  
+
+`
+const Notice = styled.text`
+  font-family: 'Mitr', sans-serif;
+  font-weight:300;
+`
 export default class Register extends React.Component {
   constructor(props) {
     super(props)
@@ -41,70 +70,100 @@ export default class Register extends React.Component {
       access_token: localStorage.access_token
     }, {responseType: "blob"})
       .then(function (response) {
-        console.log(response)
-        var url = window.URL.createObjectURL(response.data);
+        // console.log(response)
+        var url = window
+          .URL
+          .createObjectURL(response.data);
         var a = document.createElement('a');
         a.href = url;
         a.download = "cesc11-register.pdf";
         a.click();
-      }).catch(err=>{
+      })
+      .catch(err => {
         console.log(err)
       })
 
   }
   componentDidMount() {
-    console.log(localStorage.access_token)
     axios
       .post(setting.prod_api_root + '/web/profile', {access_token: localStorage.access_token})
       .then(res => {
-        // console.log(res)
         this.setState({form: res.data})
         this.forceUpdate()
-        // console.log(this.state)
       })
       .catch(err => {
-        console.log(err.response.data.message)
-        this.setState({isError: true, errorTXT: err.response.data.message})
+        // If token expired
+        if (err.response.status == 401) 
+          Router.push('/register/login')
       });
 
   }
   render() {
     return (
       <Layout>
-        <div className="text-center">
-          <Container>
-            <Row className="text-center">
-              <Col>
-                <Logo src="/static/images/hero/logo@4x.png"/> {/* <SectionHeader title="Registration form"/> */}
-              </Col>
-            </Row>
-            <Row className="text-center">
-              <Col>
-                <Button color="primary" onClick={() => this.downloadForm()}>ดาวน์โหลดใบสมัคร</Button>
-              </Col>
-              <Col>
-                <a
-                  href="https://drive.google.com/file/d/1-fuAYc7LVp1tkDmf57Eonyp6toPmK2Kv/view"
-                  target="_blank">
-                  <Button color="primary">ดาวน์โหลดคำถามทั่วไป</Button>
-                </a>
-              </Col>
-              <Col>
-                <a
-                  href={this.state.form.sub_camp == "IOT"
-                  ? "https://drive.google.com/file/d/1nqn8rKolqJEW2nUs2tUrZrqThInsTy0K/view"
-                  : "https://drive.google.com/file/d/1I50axhI5AFu7a_F5ap9Gm8qi_U5EoIgl/view"}
-                  target="_blank">
-                  <Button color="primary">ดาวน์โหลดคำถามสาขา</Button>
-                </a>
+        <PageWarper>
+          <div className="text-center">
+            <Container>
+              <Row className="text-center">
+                <Col>
+                  <Logo src="/static/images/hero/logo@4x.png"/>
+                  <SectionHeader title="Download section"/>
+                </Col>
+              </Row>
+              <Row className="text-center">
+                <Col>
+                  <Card>
+                    <CardBody>
+                      <CardRow>
+                        <Col>
+                          <Alert color="info">
+                          <i class="fas fa-info-circle"/>{' '}<Notice>น้องๆสามารถLoginเข้ามาดาวน์โหลดใบสมัครได้ตลอดระยะเวลาการรับสมัครนะครับ</Notice>
+                          </Alert>
 
-              </Col>
-            </Row>
-            <Row className="text-center"></Row>
-            <Row className="text-center"></Row>
-          </Container>
+                        </Col>
+                      </CardRow>
+                      <CardRow>
+                        <Col>
+                          <RoundButton
+                            outline
+                            color="success"
+                            onClick={() => this.downloadForm()}
+                            size="lg"
+                            block>ดาวน์โหลดใบสมัคร</RoundButton>
+                        </Col>
+                      </CardRow>
+                      <CardRow>
+                        <Col>
+                          <RoundButton
+                            outline
+                            color="success"
+                            size="lg"
+                            block
+                            href={setting.Gen_Question}
+                            target="_blank">ดาวน์โหลดคำถามทั่วไป</RoundButton>
 
-        </div>
+                        </Col>
+                      </CardRow>
+                      <CardRow>
+                        <Col>
+                          <RoundButton
+                            outline
+                            color="success"
+                            size="lg"
+                            block
+                            href={this.state.form.sub_camp == "IOT"
+                            ? setting.IOT_Question
+                            : setting.Robot_Question}
+                            target="_blank">ดาวน์โหลดคำถามสาขา</RoundButton>
+                        </Col>
+                      </CardRow>
+                    </CardBody>
+                  </Card>
+                </Col>
+              </Row>
+            </Container>
+          </div>
+        </PageWarper>
       </Layout>
     )
   }
