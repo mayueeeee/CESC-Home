6a7eda95b61c6316b0751ae860e0f5d5910d7f69
Sarunyu Chankong <sarunyu.you@gmail.com>
2018-04-01 08:28:11 +0700

    Change alert logic

--- components/Core/Alert.js
+++ components/Core/Alert.js
@@ -2,19 +2,28 @@ import React from 'react'
 import {Alert} from 'reactstrap'
 export default class Register extends React.Component {
   constructor(props) {
-    super(props)    
+    super(props)
     this.state = {
-        errorCode: this.props.errorCode||''
+      errorCode: this.props.errorCode || ''
     }
 
   }
-  
+
   render() {
     return (
-      <Alert color={this.props.color}>
-        <i className="fas fa-exclamation-circle"></i>{' '}
-        งุ้ย มีปัญหาเกิดขึ้น กรุณาลองเข้าสู่ระบบใหม่หรือติดต่อพี่ๆตามช่องทางต่างๆ ({this.props.errorCode})
-      </Alert>
+      <div>
+        {this.props.show
+          ? <Alert color={this.props.color} toggle={this.props.toggle}>
+              <i className="fas fa-exclamation-circle"></i>{' '}
+              งุ้ย มีปัญหาเกิดขึ้น กรุณาลองเข้าสู่ระบบใหม่หรือติดต่อพี่ๆตามช่องทางต่างๆ ({this.props.errorText})
+            </Alert>
+          : ""}
+        {/* <Alert color="danger" errorCode={this.state.errorTXT}/> */}
+      </div>
+    // <Alert color={this.props.color}>   <i className="fas
+    // fa-exclamation-circle"></i>{' '}   งุ้ย มีปัญหาเกิดขึ้น
+    // กรุณาลองเข้าสู่ระบบใหม่หรือติดต่อพี่ๆตามช่องทางต่างๆ ({this.props.errorCode})
+    // </Alert>
     )
   }
 }
