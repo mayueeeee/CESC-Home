bc47e10ec9d94fffb8fa940d319124e2ad3f917a
Sarunyu Chankong <sarunyu.you@gmail.com>
2021-07-28 04:45:04 +0700

    Update nextjs components

--- pages/_document.old.js
+++ pages/_document.old.js
@@ -14,16 +14,16 @@ export default class MyDocument extends Document {
     return (
       <html>
         <Head>
-              <title>CE Smart Camp 11</title>
-              <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+              {/*<title>CE Smart Camp 11</title>*/}
+              {/*<meta name="viewport" content="initial-scale=1.0, width=device-width" />*/}
               <meta property="og:title" content="CE Smart Camp 11 @ KMITL"/>              
               <meta property="og:url" content="http://cesmartcamp.com"/>
               <meta property="og:description" content="CE SMART CAMP 11เป็นค่ายอบรมเชิงปฏิบัติการเพื่อเสริมสร้างความรู้ ให้แก่น้องๆนักเรียนในระดับชั้นมัธยมศึกษาตอนปลาย"/>
               <meta property="og:type" content="company"/>
               <meta property="og:image" content="https://cesc.kmi.tl/static/images/banner.png"/>
               <link href="https://fonts.googleapis.com/css?family=Mitr:300,400" rel="stylesheet"/>
-              <link rel="stylesheet" href="/static/fonts/fonts.css"  />
-              <link rel="stylesheet" href="/static/css/styles.css" />
+              <link rel="stylesheet" href="/public/static/fonts/fonts.css"  />
+              <link rel="stylesheet" href="/public/static/css/styles.css" />
               <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.2.1/css/bootstrap.min.css" integrity="sha256-azvvU9xKluwHFJ0Cpgtf0CYzK7zgtOznnzxV4924X1w=" crossOrigin="anonymous" />
               <link rel="stylesheet" type="text/css" charSet="UTF-8" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick.min.css" /> 
               <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick-theme.min.css" /> 
@@ -32,7 +32,7 @@ export default class MyDocument extends Document {
               <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/flatpickr@4.3.2/dist/flatpickr.css"/>
               <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/flatpickr@4.3.2/dist/ie.css"/>
               <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/hover.css/2.3.1/css/hover-min.css" />
-              {this.props.styleTags}               
+              {this.props.styleTags}
 
             </Head>
         <body>
